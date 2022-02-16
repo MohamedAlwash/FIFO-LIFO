b93bf4ee6ca6f3ba6d1996c08ade2ac2cb2a76b8
MohamedAlwash <mohamedi.c.t1996@gmail.com>
2022-02-16 08:47:07 +0100

    Third commit

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Program
+{
+
+    private static List<int> array = new List<int>() { 22, 1, 58, 33 };
+
+    public static void Add(int number) {
+        array.Add(number);
+    }
+
+    public static void Remove() {
+        array.RemoveAt(0);
+    }
+
+    public static void Loop() {
+        array.ForEach(n => Console.Write(n));
+    }
+}
